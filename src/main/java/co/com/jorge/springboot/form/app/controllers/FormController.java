698ab697354bfb6b0e0fffe4d26c4fb1525fbf53
jorgevelasquezv <jorgeluisvelasquezv@gmail.com>
2022-12-16 15:43:48 -0500

    Formulario mapeado con clase pojo

--- src/main/java/co/com/jorge/springboot/form/app/controllers/FormController.java
+++ src/main/java/co/com/jorge/springboot/form/app/controllers/FormController.java
@@ -1,10 +1,10 @@
 package co.com.jorge.springboot.form.app.controllers;
 
+import co.com.jorge.springboot.form.app.models.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
@@ -16,15 +16,22 @@ public class FormController {
     }
 
     @PostMapping("/form")
-    public String sendForm(@RequestParam String username,
-                           @RequestParam String password,
-                           @RequestParam String email,
+    public String sendForm(User user,
+// La clase user se mapea automáticamente ya que los atributos coinciden con los nombres de los campos input
+//                          @RequestParam String username,
+//                           @RequestParam String password,
+//                           @RequestParam String email,
                            Model model){
+
+//        User user = new User();
+//        user.setUsername(username);
+//        user.setPassword(password);
+//        user.setEmail(email);
+
         model.addAttribute("titulo", "Resultado form");
-        model.addAttribute("username", username);
-        model.addAttribute("password", password);
-        model.addAttribute("email", email
-        );
+        model.addAttribute("user", user);
+
+
 
         return "result";
     }
